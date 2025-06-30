5b49942fc44de86854946efeca5d3b9e98cdd4d6
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-30 16:50:45 +0500

    created rating section

--- components/Rating.tsx
+++ components/Rating.tsx
@@ -27,11 +27,11 @@ const Rating = () => {
         <div className='w-full h-auto py-10 justify-center items-center flex'>
             <div className='w-[90%]  h-auto flex-wrap justify-evenly items-center flex'>
                 {rating_data.map(({ rate, type, title }, index) => (
-                    <div key={index} className='flex justify-center items-center w-auto h-auto gap-4 py-3 relative px-2'>
-                        <div className=' w-[200px] h-[200px] bg-gradient-to-r from-[#FB8FD7] via-[#FDB67F] to-[#8DECFF] blur-[70px] rotate-45 rounded-2xl '></div>
-                        <div className='w-full h-full absolute top-0 gap-4 left-0 bg-transparent flex flex-col justify-center items-center'>
+                    <div key={index} className='flex justify-center items-center w-auto h-auto gap-4 relative p-3'>
+                        <div className=' w-[200px] h-[200px] bg-gradient-to-r from-[#FB8FD7] via-[#FDB67F] to-[#8DECFF] blur-[80px] rotate-45 rounded-2xl '></div>
+                        <div className='w-full h-full absolute top-0 gap-3 left-0 bg-transparent flex flex-col justify-center items-center'>
                             <h2 className='text-5xl font-medium uppercase px-2' >{rate}{type}</h2>
-                            <p className='text-xl px-3 leading-7'>{title}</p>
+                            <p className='text-lg leading-7'>{title}</p>
                         </div>
                     </div>
                 ))}
