66938732ff04e60e061b64bf64f8c41eaca6250e
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-30 15:54:01 +0500

    creating about section

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div className='w-full h-auto flex justify-center items-center py-20'>
+      <div className='flex flex-row w-[90%] h-auto justify-center items-center gap-10'>
+        d
+      </div>
+    </div>
+  )
+}
+
+export default AboutPage
--- app/hero/page.tsx
+++ app/hero/page.tsx
@@ -1,5 +1,6 @@
 import AnimatedVector from '@/components/AnimatedVector'
 import Header from '@/components/Header'
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const HeroPage = () => {
@@ -14,12 +15,17 @@ const HeroPage = () => {
                         <AnimatedVector />
                     </div>
                     <div className='flex justify-center items-center w-full z-20 h-full absolute top-0 left-0'>
-                        <div className='flex flex-col gap-4 max-w-lg justify-center items-center text-center'>
+                        <div className='flex flex-col gap-4 max-w-2xl justify-center items-center text-center'>
                             <h1 className='text-5xl font-medium uppercase leading-16' >Discover Excellence with CodeCrafters Studio</h1>
                             <p className='text-xl px-3 leading-7'>At CodeCrafters Studio, we transform ideas into reality by delivering cutting-edge software solutions tailored to your unique needs.</p>
+                            <div className="inline-block p-[1px] bg-gradient-to-r from-[#FB8FD7] via-[#FDB67F] to-[#8DECFF] rounded-full">
+                                <button className="bg-white text-black px-8 py-2 uppercase rounded-full font-semibold">
+                                    {`Let’s Connect`}
+                                </button>
+                            </div>
                         </div>
                     </div>
-                
+
 
                 </div>
             </div>
