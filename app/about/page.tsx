af2534dd06f764a0760482c9a43672548f458dc6
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-02 00:19:24 +0500

    almost done

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,27 +4,27 @@ import React from 'react'
 
 const AboutPage = () => {
     return (
-        <div className='w-full h-auto flex justify-center items-center pt-20 md:pt-30 pb-14 md:pb-10'>
+        <div className='w-full h-auto flex justify-center items-center xs:pt-20 pt-10 md:pt-30 xs:pb-14 pb-8 md:pb-10'>
             <div className='flex md:flex-row  md:px-0 px-3 flex-col w-full md:w-[95%] lg:w-[90%] h-auto justify-center items-center gap-10'>
                 <div className='left md:w-1/2 w-full flex flex-col gap-8 px-3'>
-                    <h2 className='lg:text-5xl md:text-4xl text-3xl font-medium uppercase px-2' >Why Choose Us?</h2>
-                    <p className='md:text-xl text-lg px-3 leading-5 md:leading-7'> Our team of dedicated professionals is passionate about technology, creativity, and excellence, ensuring every project we undertake exceeds expectations.</p>
+                    <h2 className='lg:text-5xl md:text-4xl sm:text-3xl xs:text-2xl text-xl font-medium uppercase px-2' >Why Choose Us?</h2>
+                    <p className='md:text-xl xs:text-lg text-base px-3 leading-5 md:leading-7'> Our team of dedicated professionals is passionate about technology, creativity, and excellence, ensuring every project we undertake exceeds expectations.</p>
                     <div className='flex flex-col gap-2 w-full h-auto'>
                         {/* <div className="inline-block pb-[1px] bg-gradient-to-r from-[#FB8FD7] via-[#FDB67F] to-[#8DECFF]"> */}
-                            <div className=" border-style border-b text-black px-6 py-2 flex justify-between w-full items-center uppercase font-semibold">
-                                <h4 className='md:text-2xl text-xl font-medium uppercase' >Client-Centric Approach</h4>
+                            <div className=" border-style border-b text-black sm:px-6 xs:px-3 px-0 py-2 flex justify-between w-full items-center uppercase font-semibold">
+                                <h4 className='md:text-2xl sm:text-xl xs:text-lg text-base font-medium uppercase' >Client-Centric Approach</h4>
                                 <MoveRightIcon />
                             </div>
                         {/* </div> */}
                         <div className="inline-block pb-[1px] bg-gradient-to-r from-[#FB8FD7] via-[#FDB67F] to-[#8DECFF]">
-                            <div className="bg-white text-black px-6 py-2 flex justify-between w-full items-center uppercase font-semibold">
-                                <h4 className='md:text-2xl text-xl font-medium uppercase' >Innovation</h4>
+                            <div className="bg-white text-black sm:px-6 xs:px-3 px-0 py-2 flex justify-between w-full items-center uppercase font-semibold">
+                                <h4 className='md:text-2xl sm:text-xl xs:text-lg text-base font-medium uppercase' >Innovation</h4>
                                 <MoveRightIcon />
                             </div>
                         </div>
                         <div className="inline-block pb-[1px] bg-gradient-to-r from-[#FB8FD7] via-[#FDB67F] to-[#8DECFF]">
-                            <div className="bg-white text-black px-6 py-2 flex justify-between w-full items-center uppercase font-semibold">
-                                <h4 className='md:text-2xl text-xl font-medium uppercase' >Quality Assurance</h4>
+                            <div className="bg-white text-black sm:px-6 xs:px-3 px-0 py-2 flex justify-between w-full items-center uppercase font-semibold">
+                                <h4 className='md:text-2xl sm:text-xl xs:text-lg text-base font-medium uppercase' >Quality Assurance</h4>
                                 <MoveRightIcon />
                             </div>
                         </div>
--- app/hero/page.tsx
+++ app/hero/page.tsx
@@ -14,7 +14,7 @@ const HeroPage = () => {
                     <div className='w-full h-[400px] absolute left-0 top-0 flex justify-center items-start z-10'>
                         <div className=' w-[400px] h-[400px] bg-gradient-to-r from-[#FB8FD7] via-[#FDB67F] to-[#8DECFF] blur-[171px] rotate-45 rounded-4xl '></div>
                     </div>
-                    <div className='flex justify-center md:py-0 py-20 items-start w-full z-20 h-full absolute top-0 left-0'>
+                    <div className='flex justify-center md:py-0 xs:py-20 py-14 items-start w-full z-20 h-full absolute top-0 left-0'>
                         <div className='flex flex-col gap-4 max-w-2xl justify-start items-center text-center'>
                             <h1 className='md:text-5xl sm:text-4xl xs:text-3xl text-2xl font-medium uppercase xs:leading-10 leading-8 sm:leading-12 md:leading-16' >Discover Excellence with CodeCrafters Studio</h1>
                             <p className='md:text-xl xs:text-lg text-base px-3 leading-7'>At CodeCrafters Studio, we transform ideas into reality by delivering cutting-edge software solutions tailored to your unique needs.</p>
