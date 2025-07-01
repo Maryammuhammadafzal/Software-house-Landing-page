596554a2749544ecac93e07ea45328445c549e85
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-01 16:58:55 +0500

    complete

--- app/hero/page.tsx
+++ app/hero/page.tsx
@@ -10,12 +10,12 @@ const HeroPage = () => {
                 <div className="header w-full h-[80px] flex justify-center items-center">
                     <Header />
                 </div>
-                <div className='hero-section flex w-full min-h-[400px] relative justify-center items-center'>
-                    <div className='w-full h-[400px] absolute left-0 top-0 flex justify-center items-center z-10'>
+                <div className='hero-section flex w-full min-h-[400px] relative justify-center items-start'>
+                    <div className='w-full h-[400px] absolute left-0 top-0 flex justify-center items-start z-10'>
                         <div className=' w-[400px] h-[400px] bg-gradient-to-r from-[#FB8FD7] via-[#FDB67F] to-[#8DECFF] blur-[171px] rotate-45 rounded-4xl '></div>
                     </div>
-                    <div className='flex justify-center items-center w-full z-20 h-full absolute top-0 left-0'>
-                        <div className='flex flex-col gap-4 max-w-2xl justify-center items-center text-center'>
+                    <div className='flex justify-center items-start w-full z-20 h-full absolute top-0 left-0'>
+                        <div className='flex flex-col gap-4 max-w-2xl justify-start items-center text-center'>
                             <h1 className='text-5xl font-medium uppercase leading-16' >Discover Excellence with CodeCrafters Studio</h1>
                             <p className='text-xl px-3 leading-7'>At CodeCrafters Studio, we transform ideas into reality by delivering cutting-edge software solutions tailored to your unique needs.</p>
                             {/* <div className="inline-block p-[1px] bg-gradient-to-r from-[#FB8FD7] via-[#FDB67F] to-[#8DECFF] rounded-full"> */}
